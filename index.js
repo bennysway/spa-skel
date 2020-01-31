e264addaa01e709937dd98272bfed0d49fc6b74d
Ben <bcharumbira@lbpro.pl>
2020-01-31 15:57:11 +0100

    init sap spa sap

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080
+
+app.use(express.static('public'));
+
+app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+
+app.listen(port, () => {
+    console.log('listening on %d', port);
+});
